6953fb785086d1ccb2ded210684aa3370c5a5dfa
Kuisong Tong <kuisong.tong@gmail.com>
2024-01-07 03:30:13 -0800

    update license (#70)

--- internal/maps/merge_test.go
+++ internal/maps/merge_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 The konf authors
+// Copyright (c) 2024 The konf authors
 // Use of this source code is governed by a MIT license found in the LICENSE file.
 
 package maps_test
