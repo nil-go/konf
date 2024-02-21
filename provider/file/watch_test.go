14346ae844ad7898109ef79a379881b83e81825d
Kuisong Tong <kuisong.tong@gmail.com>
2024-02-20 19:42:58 -0800

    fix flaky tests (#156)

--- provider/file/watch_test.go
+++ provider/file/watch_test.go
@@ -43,6 +43,7 @@ func TestFile_Watch(t *testing.T) {
 
 			tmpFile := filepath.Join(t.TempDir(), "watch.json")
 			assert.NoError(t, os.WriteFile(tmpFile, []byte(`{"p": {"k": "v"}}`), 0o600))
+			time.Sleep(100 * time.Millisecond) // wait for the file to be written
 
 			values := make(chan map[string]any)
 
@@ -58,10 +59,10 @@ func TestFile_Watch(t *testing.T) {
 				assert.NoError(t, err)
 			}()
 			<-started
-			time.Sleep(time.Second) // wait for the watcher to start
+			time.Sleep(100 * time.Millisecond) // wait for the watcher to start
 
 			assert.NoError(t, testcase.action(tmpFile))
-			time.Sleep(time.Second) // wait for the watcher to pick up the change from action
+			time.Sleep(100 * time.Millisecond) // wait for the watcher to pick up the change from action
 			assert.Equal(t, testcase.expected, <-values)
 		})
 	}
