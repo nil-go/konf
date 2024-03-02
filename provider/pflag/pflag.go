882e861a96dc7bd63b657c8f91cf6df199be70b4
ktong <kuisong.tong@gmail.com>
2024-03-02 13:17:14 -0800

    conversion early for better performance

--- provider/pflag/pflag.go
+++ provider/pflag/pflag.go
@@ -91,7 +91,8 @@ func (f PFlag) Load() (map[string]any, error) { //nolint:cyclop
 				return
 			}
 
-			maps.Insert(values, keys, flag.Value.String())
+			val, _ := flagVal(set, flag) // Ignore error as it uses whatever returned.
+			maps.Insert(values, keys, val)
 		},
 	)
 
@@ -107,7 +108,7 @@ func zeroDefaultValue(flag *pflag.Flag) bool { //nolint:cyclop
 		return flag.DefValue == "0" || flag.DefValue == "0s"
 	case "count", "int", "int8", "int16", "int32", "int64",
 		"uint", "uint8", "uint16", "uint32", "uint64",
-		"float32", "float64":
+		"float", "float32", "float64":
 		return flag.DefValue == "0"
 	case "string":
 		return flag.DefValue == ""
@@ -125,6 +126,86 @@ func zeroDefaultValue(flag *pflag.Flag) bool { //nolint:cyclop
 		default:
 			return false
 		}
+	}
+}
+
+//nolint:cyclop,funlen,gocyclo,wrapcheck
+func flagVal(set *pflag.FlagSet, flag *pflag.Flag) (any, error) {
+	switch flag.Value.Type() {
+	case "bool":
+		return set.GetBool(flag.Name)
+	case "duration":
+		return set.GetDuration(flag.Name)
+	case "count":
+		return set.GetCount(flag.Name)
+	case "int":
+		return set.GetInt(flag.Name)
+	case "int8":
+		return set.GetInt8(flag.Name)
+	case "int16":
+		return set.GetInt16(flag.Name)
+	case "int32":
+		return set.GetInt32(flag.Name)
+	case "int64":
+		return set.GetInt64(flag.Name)
+	case "uint":
+		return set.GetUint(flag.Name)
+	case "uint8":
+		return set.GetUint8(flag.Name)
+	case "uint16":
+		return set.GetUint16(flag.Name)
+	case "uint32":
+		return set.GetUint32(flag.Name)
+	case "uint64":
+		return set.GetUint64(flag.Name)
+	case "float":
+		return set.GetFloat64(flag.Name)
+	case "float32":
+		return set.GetFloat32(flag.Name)
+	case "float64":
+		return set.GetFloat64(flag.Name)
+	case "string":
+		return set.GetString(flag.Name)
+	case "ip":
+		return set.GetIP(flag.Name)
+	case "ipMask":
+		return set.GetIPv4Mask(flag.Name)
+	case "ipNet":
+		return set.GetIPNet(flag.Name)
+	case "boolSlice":
+		return set.GetBoolSlice(flag.Name)
+	case "durationSlice":
+		return set.GetDurationSlice(flag.Name)
+	case "ipSlice":
+		return set.GetIPSlice(flag.Name)
+	case "bytesHex":
+		return set.GetBytesHex(flag.Name)
+	case "bytesBase64":
+		return set.GetBytesBase64(flag.Name)
+	case "stringArray":
+		return set.GetStringArray(flag.Name)
+	case "stringSlice":
+		return set.GetStringSlice(flag.Name)
+	case "stringToString":
+		return set.GetStringToString(flag.Name)
+	case "stringToInt":
+		return set.GetStringToInt(flag.Name)
+	case "stringToInt64":
+		return set.GetStringToInt64(flag.Name)
+	case "intSlice":
+		return set.GetIntSlice(flag.Name)
+	case "int32Slice":
+		return set.GetInt32Slice(flag.Name)
+	case "int64Slice":
+		return set.GetInt64Slice(flag.Name)
+	case "uintSlice":
+		return set.GetUintSlice(flag.Name)
+	case "float32Slice":
+		return set.GetFloat32Slice(flag.Name)
+	case "float64Slice":
+		return set.GetFloat64Slice(flag.Name)
+	default:
+		return flag.Value.String(), nil
 	}
 }
 
